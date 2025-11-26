9669065af4028fd4f5ee64206ecb9e2149a93559
Patiwat <patiwat_p@hongsapower.com>
2025-11-26 13:13:21 +0700

    Create frontend

---
+++ frontend/src/pages/Service.tsx
@@ -0,0 +1,11 @@
+
+function Service() {
+  return (
+    <div className="p-8">
+      <h2 className="text-2xl font-bold mb-4">Service Page</h2>
+      <p>If you have any questions, feel free to reach out!</p>
+    </div>
+  )
+}
+
+export default Service
